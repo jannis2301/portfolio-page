f610e2fb87576aba9c1a4263056de09f0a0f2a24
jannis2301 <jannis.ax@gmx.de>
2023-02-25 14:05:31 +0100

    paths updated

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="portfolio" element={<Portfolio />} />
-          <Route path="contact" element={<Contact />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/contact" element={<Contact />} />
           <Route path="*" element={<Error />} />
         </Route>
       </Routes>
