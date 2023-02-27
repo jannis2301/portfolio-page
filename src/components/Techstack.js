57be4c5d0f21ca55975be59735ba4bfa09046d56
jannis2301 <jannis.ax@gmx.de>
2023-02-27 14:04:41 +0100

    add flip cards animation

--- src/components/Techstack.js
+++ src/components/Techstack.js
@@ -1,15 +1,54 @@
+import { useEffect, useState, useRef } from 'react'
+import debounce from '../utils/debounce'
+
 const Techstack = ({ technologies }) => {
+  const [clickedCard, setClickedCard] = useState(null)
+  const [shouldHideOverflow, setShouldHideOverflow] = useState(false)
+  const techstackRef = useRef(null)
+
+  const handleClickOutside = (event) => {
+    if (techstackRef.current && !techstackRef.current.contains(event.target)) {
+      setClickedCard(null)
+    }
+  }
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setShouldHideOverflow(true)
+    }, 2000) // adjust the timeout as needed
+
+    window.addEventListener('click', handleClickOutside)
+
+    return () => {
+      clearTimeout(timeoutId)
+      window.removeEventListener('click', handleClickOutside)
+    }
+  }, [])
+
   return (
-    <div className="techstack-box">
+    <div className="techstack-box" ref={techstackRef}>
       {technologies.map((tech, id) => {
-        const { icon } = tech
+        const { icon, title } = tech
+        const isClicked = clickedCard === id
+
+        const handleCardClick = debounce((id) => {
+          setClickedCard(isClicked ? null : id)
+        }, 200)
+
         return (
-          <div className="tech-cards" key={id}>
-            {icon}
+          <div
+            className={`tech-cards ${isClicked ? 'flip' : ''}`}
+            key={id}
+            onClick={() => handleCardClick(id)}
+            style={{ overflow: shouldHideOverflow ? 'hidden' : 'visible' }}
+          >
+            <span className="front-face">{icon}</span>
+            <span className="back-face">{title}</span>
           </div>
         )
       })}
     </div>
   )
 }
+
 export default Techstack
--- src/data/technologies.js
+++ src/data/technologies.js
@@ -9,21 +9,27 @@ import {
 
 const technologies = [
   {
+    title: 'HTML5',
     icon: <FaHtml5 />,
   },
   {
+    title: 'CSS3',
     icon: <FaCss3 />,
   },
   {
+    title: 'JavaScript',
     icon: <FaJsSquare />,
   },
   {
+    title: 'ReactJS',
     icon: <FaReact />,
   },
   {
+    title: 'NodeJS',
     icon: <FaNode />,
   },
   {
+    title: 'Git',
     icon: <FaGitAlt />,
   },
 ]
